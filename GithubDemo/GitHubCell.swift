487cd24cb009e5971af357fc9c20e75537543ac0
EricSFChu <echu6@mail.ccsf.edu>
2016-02-25 20:19:42 -0800

    Milestone Three

---
+++ GithubDemo/GitHubCell.swift
@@ -0,0 +1,20 @@
+//
+//  GitHubCell.swift
+//  GithubDemo
+//
+//  Created by EricDev on 2/25/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class GitHubCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var owner: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var stars: UILabel!
+    @IBOutlet weak var forks: UILabel!
+    @IBOutlet weak var repoDescription: UILabel!
+    
+}
--- GithubDemo/RepoResultsViewController.swift
+++ GithubDemo/RepoResultsViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import MBProgressHUD
+import AFNetworking
 
 // Main ViewController
-class RepoResultsViewController: UIViewController {
+class RepoResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
     var searchBar: UISearchBar!
     var searchSettings = GithubRepoSearchSettings()
 
@@ -19,7 +21,8 @@ class RepoResultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.dataSource = self
+        
         // Initialize the UISearchBar
         searchBar = UISearchBar()
         searchBar.delegate = self
@@ -29,7 +32,9 @@ class RepoResultsViewController: UIViewController {
         navigationItem.titleView = searchBar
 
         // Perform the first search when the view controller first loads
+        self.tableView.reloadData()
         doSearch()
+        
     }
 
     // Perform the search.
@@ -42,13 +47,44 @@ class RepoResultsViewController: UIViewController {
 
             // Print the returned repositories to the output window
             for repo in newRepos {
-                print(repo)
-            }   
-
+                //print(repo)
+            }
+            self.repos = newRepos
+            print(" MY REPOS ", self.repos)
+            
+            self.tableView.reloadData()
             MBProgressHUD.hideHUDForView(self.view, animated: true)
             }, error: { (error) -> Void in
                 print(error)
         })
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("GitCell") as! GitHubCell
+        let repo = self.repos![indexPath.row]
+        cell.name.text = repo.name
+        cell.name.sizeToFit()
+        cell.owner.text = repo.ownerHandle
+        cell.owner.sizeToFit()
+        cell.stars.text = "\(repo.stars)"
+        cell.stars.sizeToFit()
+        cell.forks.text = "\(repo.forks)"
+        cell.forks.sizeToFit()
+        cell.repoDescription.text = repo.repoDescriptionField
+        cell.repoDescription.sizeToFit()
+        cell.profileImage.setImageWithURL(NSURL(string: repo.ownerAvatarURL!)!)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let repos = repos {
+            return repos.count
+        }
+        else {
+            
+        return 0
+        }
     }
 }
 
